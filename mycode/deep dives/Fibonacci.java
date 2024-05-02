69445ac4bd54aacf26d1d25ba3b2d5b30f798862
TMoreki <tlotlegi.moreki@gmail.com>
2024-05-02 11:35:51 +0200

    Update Fibonacci.java

--- mycode/deep dives/Fibonacci.java
+++ mycode/deep dives/Fibonacci.java
@@ -1,16 +1,47 @@
-public class Fibonacci {
+class Fibonacci { //recursive method
     public static void main(String[] args) {
-        int n = 20; // Number of Fibonacci numbers to print
-        int num1 = 0, num2 = 1;
+        int n = 10; 
+        printFibonacciSeries(n);
+    }
 
-        System.out.println(num1); // Print the first Fibonacci number
-        System.out.println(num2); // Print the second Fibonacci number
+    static void printFibonacciSeries(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(fibonacci(i));
+        }
+    }
 
-        for (int i = 2; i < n; i++) {
-            int nextNum = num1 + num2;
-            System.out.println(nextNum);
-            num1 = num2;
-            num2 = nextNum;
+    static int fibonacci(int n) {
+        
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+        
+        else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
+
+
+// Alternative 
+
+class Fibonacci { // iterative method 
+    public static void main(String[] args) {
+        int n = 11; // Number of Fibonacci numbers to print
+        printFibonacciSeries(n);
+    }
+
+    static void printFibonacciSeries(int n) {
+        int a = 0, b = 1;
+        System.out.println(a); // Print the first Fibonacci number (0)
+
+        for (int i = 1; i < n; i++) {
+            System.out.println(b); // Print the next Fibonacci number
+            int temp = b;
+            b = a + b;
+            a = temp;
         }
     }
 }
