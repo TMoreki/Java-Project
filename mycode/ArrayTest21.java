635a7f89a5b664610ad8d5636a89edae283ab85d
TMoreki <tlotlegi.moreki@gmail.com>
2024-05-07 15:26:22 +0200

    Create ArrayTest21.java

---
+++ mycode/ArrayTest21.java
@@ -0,0 +1,43 @@
+// public class ArrayTest21 {
+// 	public static void main(String[] args)
+// 	{
+
+// 	    int i = 1;
+// 	    int [] iArr = {1};
+// 	    incr(i);
+// 	    incr(iArr);
+// 	    System.out.println("i = " + i + " iArr[0]" + iArr[0]);
+
+// 	}
+
+// 	public static void incr (int n) {n++; }
+// 	public static void incr (int[] n) { n[0]++; }
+
+	
+// }
+
+// public class ArrayTest21{
+// 	public static void main(String[] args){
+// 		if(args[0].equals("open"))
+// 			if(args[1].equals("someone"))
+// 				System.out.println("Hello!");
+// 		else System.out.println("Go away " + args [1]);
+// 	}
+// }
+
+
+// public class ArrayTest21{
+// 	public static void main(String [] args){
+// 		int i = Integer.parseInt(args[1]);
+// 		System.out.println(args[i]);
+// 	}
+// }
+
+public class ArrayTest21{
+	public static void main(String[] args){
+		ArrayTest21 t = new ArrayTest21();
+		int[] arr = new int[10];
+		arr = t.subArray(arr,0,2);
+	}
+	public int[] subArray(int[] src, int start,int end){return src;}
+}
